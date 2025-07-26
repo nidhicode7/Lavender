4f96d93f9421ba1e42df36a7494f4457d86bf94f
Nidhishaa Ardham <166903003+nidhicode7@users.noreply.github.com>
2025-07-26 23:06:23 +0530

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,10 @@
+# Program to calculate factorial of a number using a loop
+
+n = int(input("Enter a number: "))
+fact = 1
+
+# Multiply all numbers from 1 to n
+for i in range(1, n + 1):
+    fact *= i
+
+print("Factorial:", fact)
